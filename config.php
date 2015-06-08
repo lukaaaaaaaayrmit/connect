ef4afdf7181f60dc8f622df9d7fac520b8d7d28f
Ubuntu <ubuntu@ip-172-31-1-36.ap-southeast-2.compute.internal>
2015-06-08 03:37:04 +0000

    new config file

---
+++ config.php
@@ -0,0 +1,10 @@
+/*****************************/
+/* FILE: config.php          */
+/* AUTHOR: Luke Phelan       */
+/* STUDENT NUMBER: s3197473  */
+/*****************************/
+
+<?php 
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL | E_STRICT);
+?> 
